88aa98fe99d2678e88a42eea6c65ef345e6c652b
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-11-21 22:41:06 +0530

    Solution of B problem

---
+++ Codeforces Round #835 (Div. 4)/B_problem.cpp
@@ -0,0 +1,43 @@
+// 1st method by tourist
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << (int) (*max_element(s.begin(), s.end()) - 'a' + 1) << '\n';
+  }
+  return 0;
+}
+
+//2nd method by me
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int n; cin >> n;
+  string s; cin >> s;
+  sort(s.begin(), s.end());
+  char ch = s[n - 1];
+  cout << int(ch) - 96 << endl;
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tt; cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
