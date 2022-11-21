0a9f54704aa92f82244e3420849c35177b0e0b0e
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-11-21 22:38:49 +0530

    Solution of A problem

---
+++ Codeforces Round #835 (Div. 4)/A_Problem.cpp
@@ -0,0 +1,52 @@
+// 1st method by tourist
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    if (b > c) swap(b, c);
+    cout << b << '\n';
+  }
+  return 0;
+}
+
+// 2nd method by me
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  vector<int>arr;
+  arr.push_back(a);
+  arr.push_back(b);
+  arr.push_back(c);
+  sort(arr.begin(), arr.end());
+  cout << arr[1] << endl;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tt; cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
