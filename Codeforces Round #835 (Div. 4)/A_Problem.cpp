4c93e2561e9ec923b1dba0037db7e8eda6125916
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-11-22 22:15:41 +0530

    Update A_Problem.cpp

--- Codeforces Round #835 (Div. 4)/A_Problem.cpp
+++ Codeforces Round #835 (Div. 4)/A_Problem.cpp
@@ -2,13 +2,7 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
- 
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
