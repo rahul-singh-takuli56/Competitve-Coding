963df60d4db40feef145b56cc80d497c471e7d21
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-11-23 19:16:12 +0530

    Create main.cpp

---
+++ Contest_Template/main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define vec vector<ll>
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+#define f(i,n) for(int i=0;i<n;i++)
+#define pb push_back()
+#define pob pop_back()
+#define inf 1e9
+
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  ll tt; cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
